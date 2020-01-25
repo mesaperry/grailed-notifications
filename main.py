18e7bcc694f336234c818904ac993e9e8e7fcaae
Mesa Perry <mesperrus@gmail.com>
2020-01-25 02:39:11 -0600

    initial selenium attempt

---
+++ main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# import argparse
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("search_url")
+# args = parser.parse_args()
+# print("Parsing " + args.search_url)
+
+# URL = args.search_url
+
+driver = webdriver.Firefox()
