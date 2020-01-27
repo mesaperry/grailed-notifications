d195e21349755eae67fcca7f246ee0c08185fd1a
Mesa Perry <mesperrus@gmail.com>
2020-01-27 02:47:12 -0600

    semi working console notifications

--- app.py
+++ app.py
@@ -1,1 +1,41 @@
+from selenium import webdriver
+import pickle
+import os
+import time
+
 from getlistings import getListings
+from sendemail import sendEmail
+
+
+if __name__ == "__main__":
+    SAVED_DATA = "listing.data"
+    with open("searches.txt") as f:
+        searches = f.read().splitlines()
+
+    if os.path.isfile(SAVED_DATA):
+        os.remove(SAVED_DATA)
+
+    count = 0
+    driver = webdriver.Firefox()
+    while(count < 20):
+        # create listing data
+        listing_data = []
+        for search_url in searches:
+            listing_data.append(getListings(driver, search_url))
+
+        # check current listings against old listings
+        if os.path.isfile(SAVED_DATA):
+            with open(SAVED_DATA, "rb") as filehandle:
+                old_listings = pickle.load(filehandle)
+                for i in range(len(searches)):
+                    for listing in listing_data[i]:
+                        if listing not in old_listings[i]:
+                            count += 1
+                            print("new_listing found")
+
+        # save listings to file
+        with open(SAVED_DATA, "wb") as filehandle:
+            pickle.dump(listing_data, filehandle)
+
+
+        time.sleep(5 * 60)
--- getlistings.py
+++ getlistings.py
@@ -1,21 +1,22 @@
-from selenium import webdriver
-import time
-
-
 # def totalListings():
 #     num_xpath = "//div[contains(@class, 'ais-Panel -stats')]/div[contains(@class, 'ais-Panel-body')]/span"
 #     return int(driver.find_element_by_xpath(num_xpath).text.split()[0])
 
-def getListings(search_page):
+def getListings(driver, search_page):
+    import time
     # referenced from:
     # https://stackoverflow.com/questions/20986631/how-can-i-scroll-a-web-page-using-selenium-webdriver-in-python
     def scrollToBottom():
-        SCROLL_PAUSE_TIME = 0.5
+        SCROLL_PAUSE_TIME = 2
 
         # Get scroll height
         last_height = driver.execute_script("return document.body.scrollHeight")
 
         while True:
+            # Closer login popup
+            if driver.find_elements_by_class_name("close"):
+                driver.find_element_by_class_name("close").click()
+
             # Scroll down to bottom
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
@@ -33,26 +34,18 @@ def getListings(search_page):
         return driver.find_elements_by_class_name("feed-item")
 
 
-    driver = webdriver.Firefox()
     driver.get(search_page)
     if "Page Not Found" in driver.title:
         print("Invalid search url")
         driver.close()
         quit()
 
-    time.sleep(1)
-    driver.find_element_by_xpath("//span[text()='Buyer Protection & Authenticity Checks']").click() # summon login popup
-    driver.find_element_by_class_name("close").click() # close popup
+    # attempt to summon and close login popup
+    for _ in range(3):
+        driver.find_element_by_xpath("//span[text()='Buyer Protection & Authenticity Checks']").click()
 
     listings_elements = getListingsElements()
     listings = []
     for listing_element in listings_elements:
         listings.append(listing_element.find_element_by_tag_name("a").get_attribute("href"))
     return listings
-
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("search_url")
-    URL = parser.parse_args().search_url
-    print(getListings(URL))
